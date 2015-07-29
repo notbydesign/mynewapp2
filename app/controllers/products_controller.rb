2028de6812387127410c826b6cc1b9335279446f
Dion <notbydesign029@gmail.com>
2015-07-29 17:38:17 +0000

    Added Image Carousel

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,8 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.all
+    @products = Product.limit(3)
+    render layout: "products"
   end
 
   # GET /products/1
