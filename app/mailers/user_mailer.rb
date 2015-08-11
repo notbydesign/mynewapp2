be84065f900097dce21ec84118031e1dad865b17
Dion <notbydesign029@gmail.com>
2015-08-11 04:49:25 +0000

    fixed mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,10 @@
 class UserMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "dfudge_Online@yahoo.com"
 
   def contact_form(email, name, message)
   	@message = message
   	mail(:from => email,
-  		          :to => 'your-email@example.com',
+  		          :to => 'dfudge_Online@yahoo.com',
   		          :subject => "A new contact form message from #{name}")
  end  	
 end
