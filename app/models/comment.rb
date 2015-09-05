15362efe1ec37860e9c149db3e767399b635c56f
Dion <notbydesign029@gmail.com>
2015-09-05 20:04:58 +0000

    Will paginate fix

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,8 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :product
 
+  self.per_page = 5
+
   validates :body, presence: true
   validates :user, presence: true
   validates :product, presence: true
