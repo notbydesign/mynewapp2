d2ba9f4729f7b665b9e6a4626d9edc320c1de63f
Dion <notbydesign029@gmail.com>
2015-09-26 19:05:14 +0000

    commented out env key

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,8 +1,8 @@
-if Rails.env.production?
-	Rails.configuration.stripe = {
-		:publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
-		:secret_key => ENV['STRIPE_SECRET_KEY']
-	}
+# if Rails.env.production?
+#	Rails.configuration.stripe = {
+#		:publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
+#		:secret_key => ENV['STRIPE_SECRET_KEY']
+#	}
 
 else
 	Rails.configuration.stripe = {
